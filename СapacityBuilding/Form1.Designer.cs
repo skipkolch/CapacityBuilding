ffaf581ffd3f4761ef6d107a1001a2d2c8b749b5
Evgenii <48kea48@gmail.com>
2018-11-02 16:04:25 +0300

    first commit

--- СapacityBuilding/Form1.Designer.cs
+++ СapacityBuilding/Form1.Designer.cs
@@ -28,13 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.parseDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.averageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.channelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.а1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zA2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.o2A2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.channelToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(555, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importToolStripMenuItem,
+            this.exportToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.importToolStripMenuItem.Text = "Import...";
+            this.importToolStripMenuItem.Click += new System.EventHandler(this.importToolStripMenuItem_Click);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.parseDataToolStripMenuItem,
+            this.averageToolStripMenuItem});
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exportToolStripMenuItem.Text = "Export...";
+            // 
+            // parseDataToolStripMenuItem
+            // 
+            this.parseDataToolStripMenuItem.Name = "parseDataToolStripMenuItem";
+            this.parseDataToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.parseDataToolStripMenuItem.Text = "ParseData";
+            this.parseDataToolStripMenuItem.Click += new System.EventHandler(this.parseDataToolStripMenuItem_Click);
+            // 
+            // averageToolStripMenuItem
+            // 
+            this.averageToolStripMenuItem.Name = "averageToolStripMenuItem";
+            this.averageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.averageToolStripMenuItem.Text = "Average";
+            this.averageToolStripMenuItem.Click += new System.EventHandler(this.averageToolStripMenuItem_Click);
+            // 
+            // channelToolStripMenuItem
+            // 
+            this.channelToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.а1ToolStripMenuItem,
+            this.zA2ToolStripMenuItem,
+            this.o2A2ToolStripMenuItem});
+            this.channelToolStripMenuItem.Name = "channelToolStripMenuItem";
+            this.channelToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.channelToolStripMenuItem.Text = "Channel";
+            // 
+            // а1ToolStripMenuItem
+            // 
+            this.а1ToolStripMenuItem.Name = "а1ToolStripMenuItem";
+            this.а1ToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.а1ToolStripMenuItem.Text = "O1А1";
+            this.а1ToolStripMenuItem.Click += new System.EventHandler(this.а1ToolStripMenuItem_Click);
+            // 
+            // zA2ToolStripMenuItem
+            // 
+            this.zA2ToolStripMenuItem.Name = "zA2ToolStripMenuItem";
+            this.zA2ToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.zA2ToolStripMenuItem.Text = "OzA2";
+            this.zA2ToolStripMenuItem.Click += new System.EventHandler(this.zA2ToolStripMenuItem_Click);
+            // 
+            // o2A2ToolStripMenuItem
+            // 
+            this.o2A2ToolStripMenuItem.Name = "o2A2ToolStripMenuItem";
+            this.o2A2ToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.o2A2ToolStripMenuItem.Text = "O2A2";
+            this.o2A2ToolStripMenuItem.Click += new System.EventHandler(this.o2A2ToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(0, 24);
+            this.chart1.Name = "chart1";
+            series5.BorderWidth = 2;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.Color = System.Drawing.Color.Red;
+            series5.Legend = "Legend1";
+            series5.Name = "Values";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(555, 322);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 336);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(555, 10);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(555, 346);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem parseDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem averageToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem channelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem а1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zA2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem o2A2ToolStripMenuItem;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- СapacityBuilding/Form1.cs
+++ СapacityBuilding/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,238 @@ namespace СapacityBuilding
 {
     public partial class Form1 : Form
     {
+        private int NumberChannel;
+
+        List<List<double>> data;
+
+        private bool CanStartWrite;
+
+        private double[] averageValues;
+
+        private StreamWriter streamWriter;
+
         public Form1()
         {
             InitializeComponent();
+
+            NumberChannel = 0;
+
+            CanStartWrite = false;
+
+            openFileDialog1.Filter = saveFileDialog1.Filter = "Text files(*.txt)|*.txt";
+        }
+           
+
+        private void importToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (NumberChannel != 0)
+            {
+                Stream mystr = null;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if ((mystr = openFileDialog1.OpenFile()) != null)
+                    {
+                        StreamReader myread = new StreamReader(mystr);                       
+                        try
+                        {
+                            data = ReadingFromFile(myread);
+
+                            averageValues = CalculateAverageValues(data);
+
+                            plotGraphic(averageValues);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                        finally
+                        {
+                            mystr.Close();
+                            myread.Close();
+                        }
+                    }
+                }                
+            }
+            else MessageBox.Show("Input Number Channel");
+        }
+
+        private void plotGraphic(double[] arr)
+        {
+            chart1.Series["Values"].Points.Clear();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                chart1.Series["Values"].Points.AddXY(i, arr[i]);
+            }
+        }
+
+        private double[] CalculateAverageValues(List<List<double>> array)
+        {
+            double[] local = new double[array[0].Count];
+
+            double sum = 0;
+
+            progressBar1.Value = 0;
+            progressBar1.Maximum = local.Length;
+            
+            for (int i = 0; i < local.Length; i++ , progressBar1.Value++)
+            {
+                try
+                {
+                    for (int j = 0; j < array.Count; j++)
+                    {
+                        sum += array[j][i];
+                    }
+
+                    sum /= array.Count;
+                    local[i] = sum;
+
+                    sum = 0;
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    break;
+                }
+
+            }        
+            
+            return local;
+        }
+
+        private List<List<double>> ReadingFromFile(StreamReader reader)
+        {
+            List<List<double>> local = new List<List<double>>();
+            string[] lineData;
+
+            int i = -1;
+            CanStartWrite = false;
+
+            while (!reader.EndOfStream)
+            {
+                lineData = reader.ReadLine().Split(' ');
+              
+                if (lineData[0] == "0")
+                {
+                    if (CanStartWrite)
+                        local[i].Add(double.Parse(lineData[NumberChannel]));
+                    else continue;
+                }
+                else
+                {
+                    local.Add(new List<double>());
+                    CanStartWrite = true;
+                    i++;
+                }
+                            
+            }
+            local.Remove(local[local.Count - 1]);
+
+            return local;
+        }
+
+        private void а1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NumberChannel = 1;
+        }
+
+        private void zA2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NumberChannel = 2;
+        }
+
+        private void o2A2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NumberChannel = 3;
+        }
+
+        private void parseDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFiles(data);
+        }
+
+
+        private void saveFiles(double[] arr)
+        {
+            if (arr != null)
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                    return;
+
+                try
+                {// получаем выбранный файл
+                    string filename = saveFileDialog1.FileName;
+
+                    streamWriter = new StreamWriter(new FileStream(filename, FileMode.Create));
+                    // сохраняем текст в файл
+                    for (int i = 0; i < arr.Length; i++)
+                        streamWriter.WriteLine(arr[i]);
+
+                    MessageBox.Show("Файл сохранен");
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                catch (FormatException ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                finally
+                {
+                    streamWriter.Close();
+                }
+            }        
+            else MessageBox.Show("Please importing data");
+        }
+
+        private void saveFiles(List<List<double>> arr)
+        {
+            if (arr != null)
+            {
+                if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                    return;
+                // получаем выбранный файл
+                string filename = saveFileDialog1.FileName;
+                try
+                {
+                    streamWriter = new StreamWriter(new FileStream(filename, FileMode.Create));
+                    // сохраняем текст в файл
+
+                    string strforSave;
+
+                    for (int i = 0; i < arr[0].Count; i++)
+                    {
+                        strforSave = "";
+                        for (int j = 0; j < arr.Count; j++)
+                        {
+                            strforSave += arr[j][i].ToString() + "\t";
+                        }
+
+                        streamWriter.WriteLine(strforSave);
+                    }
+
+                    MessageBox.Show("Файл сохранен");
+                }
+                catch (IOException ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                catch (FormatException ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+                finally
+                {
+                    streamWriter.Close();
+                }
+            }
+            else MessageBox.Show("Please importing data");
+        }
+
+        private void averageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFiles(averageValues);
         }
     }
 }
